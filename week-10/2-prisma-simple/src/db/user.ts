e5e0d101e5d7879a94982f3b00f02fdbfde573b8
ganeshrevadi <ganeshrevadi16@gmail.com>
2024-02-07 22:34:18 +0530

    Postgres

--- week-10/2-prisma-simple/src/db/user.ts
+++ week-10/2-prisma-simple/src/db/user.ts
@@ -13,7 +13,16 @@ const prisma = new PrismaClient();
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+  const res = await prisma.user.create({
+    data: {
+      username,
+      password,
+      name
+    }
+  })
+  console.log(res);
+
+  return res
 }
 
 /*
@@ -25,5 +34,12 @@ export async function createUser(username: string, password: string, name: strin
  * }
  */
 export async function getUser(userId: number) {
-    
+  const res = await prisma.user.findUnique({
+    where: {
+      id: userId
+    }
+  })
+  console.log(res);
+
+  return res;
 }
